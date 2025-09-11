ee1644f9bac8532818da2a1795e1d1be35f6b943
Vitaliy <Vitaliy_Matkovskiy@mail.ru>
2025-09-11 22:43:38 +0300

    доработать добавление блюда

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -10,8 +10,8 @@ public class DinnerConstructor {
     Random random = new Random(); //этот вспомогательный класс поможет сделать произвольные сочетания блюд
 
     //в этом методе мы добавляем компонент в подборку
-    public void addNewDish(String dishType, String dishName) {
-        ArrayList<String> dishesForType = null; //переменая для списка блюд \\ НУЖЕН ЛИ НУЛ??
+    public  void addNewDish(String dishType, String dishName) {
+        ArrayList<String> dishesForType = new ArrayList<>() ; //переменая для списка блюд \\ НУЖЕН ЛИ НУЛ??
         if (dinnersByType.containsKey(dishType)) { //здесь мы должны проверить, содержит ли наше хранилище такое блюдо
             dishesForType = dinnersByType.get(dishType);//если мы уже работали с этим типом - используем существующий список
         } else {
@@ -22,37 +22,39 @@ public class DinnerConstructor {
         dishesForType.add(dishName); //независимо от того, новый это список или существующий - добавим в него конкретное блюдо
     }
 
+    //метод для проверки дубликатов блюд
+    public boolean checkType(String type) {
+        return dinnersByType.containsKey(type); //если хранилище уже содержит такое блюдо - вернём true
+    }
+
     //метод для генерирования вариантов комбинации блюд
     public ArrayList<ArrayList<String>> generateCombos(int comboNumber, ArrayList<String> dishTypes) {
-        ??? combos = ??? //пустой список для хранения получившихся комбинаций блюд
-        for (???; i <= comboNumber; ???) {
-            ??? combo = generateCombo(dishTypes); //одна комбинация блюд генерируется в отдельном методе
+        ArrayList<ArrayList<String>> combos = new ArrayList<>(); //пустой список для хранения получившихся комбинаций блюд
+        for (int i=1; i <= comboNumber; i++) {
+            ArrayList<String> combo = generateCombo(dishTypes); //одна комбинация блюд генерируется в отдельном методе
             combos.add(combo);
         }
         return combos;
     }
 
 
-    //метод для проверки дубликатов блюд
-    public boolean checkType(String type) {
-        return dinnersByType.???(type); //если хранилище уже содержит такое блюдо - вернём true
-    }
-
     //метод для генерирования одной комбинации блюд
-    private ??? ***(??? dishTypes) {
-        ??? selectedDishes = ???;
-        for (??? dishType: dishTypes) {
-            ??? availableDishes = dinnersByType.???(dishType); //достаём из хранилища варианты блюд по типу
-            String selectedDish = ???(availableDishes); //полцчим произвольное блюдо
-            selectedDishes.???(selectedDish); //добавим блюдо в подборку комбинацию
+    private ArrayList<String> generateCombo(ArrayList<String> dishTypes) {
+        ArrayList<String> selectedDishes = new ArrayList<>();
+        for (String dishType: dishTypes) {
+            ArrayList<String> availableDishes = dinnersByType.get(dishType); //достаём из хранилища варианты блюд по типу
+            String selectedDish = getRandomDish(availableDishes); //полцчим произвольное блюдо
+            //String selectedDish = availableDishes.get(random.nextInt(availableDishes.size()))--зачем плодить методы??
+            selectedDishes.add(selectedDish); //добавим блюдо в подборку комбинацию
         }
         return selectedDishes;
     }
 
-    private String getRandomDish(??? availableDishes) {
+    private String getRandomDish(ArrayList<String> availableDishes) {
         int numberOfDishesForType = availableDishes.size(); //получаем общее количество доступных блюд этого типа
         int dishIndex = random.nextInt(numberOfDishesForType); //генерируем случайное число от 0 до (кол-во блюд - 1), чтобы выбрать случайное блюдо
-        String selectedDish = availableDishes.???(dishIndex); //выберем произвольное блюдо по индексу
+        String selectedDish = availableDishes.get(dishIndex); //выберем произвольное блюдо по индексу
+        return selectedDish;
     }
 
 }
